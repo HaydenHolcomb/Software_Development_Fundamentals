3f535248ffa354c43aeddc67c92d5cd999c1c74f
Hayden <hayden.ian@outlook.com>
2019-01-03 16:59:38 -0600

    Created InheritanceDemo files

---
+++ InheritanceDemo2.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVA_Class_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog Spot = new Dog();
+            Spot.Color = "Brown";
+        }
+    }
+}
